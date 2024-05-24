9f5029133961a396dfdc9d6dc4c295b9cf12ec60
Nikita6813 <155742594+Nikita6813@users.noreply.github.com>
2024-05-24 13:38:39 +0530

    Initial commit

---
+++ Practice_ex/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //.............Addition of two numbers........//
+        int a, b, c;
+        a = 10;
+        b = 20;
+        c = a + b;
+        Console.WriteLine("Addition of a+b= " + c);
+
+
+
+
+    }
+}
